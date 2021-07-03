2731e5071719b06ee5fbb0391764ce5b7e053784
zeplar exe <49086150+zeplar-exe@users.noreply.github.com>
2021-07-03 13:06:21 -0700

    Added version command. 'jp -v|--version'

--- jectionpro-CLI/InterfaceClasses/RootCommandBuilder.cs
+++ jectionpro-CLI/InterfaceClasses/RootCommandBuilder.cs
@@ -1,10 +1,14 @@
+using System;
 using System.CommandLine;
+using System.CommandLine.Invocation;
 using System.Text;
 
 namespace jectionpro_CLI.InterfaceClasses
 {
     public class RootCommandBuilder : RootCommand
     {
+        private const string Version = "1.1.2";
+        
         public RootCommandBuilder()
         {
             var descBuilder = new StringBuilder("jectionpro CLI");
@@ -14,6 +18,19 @@ namespace jectionpro_CLI.InterfaceClasses
             Add(ProjectCommand.GetCommand());
             Add(ListCommand.GetCommand());
             Add(PinCommand.GetCommand());
+
+            Add(new Option(
+                new[] {"-v", "--version"},
+                "Displays the current CLI version",
+                typeof(bool)));
+
+            Handler = CommandHandler.Create<bool>(RootHandler);
+        }
+
+        private void RootHandler(bool version)
+        {
+            if (version)
+                Console.WriteLine(Version);
         }
     }
 }
